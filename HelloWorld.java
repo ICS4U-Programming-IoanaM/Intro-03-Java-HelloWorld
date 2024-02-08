ac2c7ccf4346d3f0c74ecd181fb5cc27e28caae8
Ioana Marinescu <ioana.marinescu@stu.ocsb.ca>
2024-02-08 13:47:01 +0000

    completed HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+class Main {
+    public static void main(String[] args) {
+        // display to console
+        System.out.println("Hey pookie <3");
+        System.out.println("I miss you!");
+    }
+}
